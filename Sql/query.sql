6267706b76e44f5a482980f4a8e0d42a27725dc7
tahmee <susantamunokubie@gmail.com>
2025-10-22 09:12:11 +0100

    Modify query file and add query 4

--- Sql/query.sql
+++ Sql/query.sql
@@ -1,12 +1,14 @@
+-- ----------------------------------------------
 -- Count the total number of customers who joined in 2023
-
+-- ----------------------------------------------
 SELECT COUNT(customer_id) AS total_customer_2023
 FROM customers
 WHERE join_date BETWEEN '2023-01-01' AND '2023-12-31';
 
-
--- For each customer return customer_id, full_name, total_revenue(sum of total_amount from orders). Sort descending
-
+-- ----------------------------------------------
+/* For each customer return customer_id, full_name, total_revenue(sum of total_amount from orders). 
+Sort descending */
+-- ----------------------------------------------
 SELECT c.customer_id,
     c.full_name,
     SUM(o.total_amount) AS total_revenue
@@ -16,8 +18,9 @@ USING (customer_id)
 GROUP BY c.customer_id, c.full_name
 ORDER BY total_revenue DESC;
 
+-- ----------------------------------------------
 -- Return the top 5 customers by total_revenue with their rank
-
+-- ----------------------------------------------
 WITH revenue_per_customer AS (
     -- Calculate total revenue for each customer
     SELECT c.customer_id,
@@ -41,15 +44,39 @@ SELECT full_name,
     customer_rank
 FROM ranked_customer_revenue
 WHERE customer_rank <= 5
+-- sort records in ascending order for customer_rank (i.e 1, 2 ..) and descending order for total_revenue
 ORDER BY customer_rank, total_revenue DESC;
 
-
+-- ----------------------------------------------
 -- Produce a table with year, month, monthly_revenue for all months in 2023 ordered chronologically
+-- ----------------------------------------------
 SELECT EXTRACT (YEAR FROM order_date) AS order_year,
     EXTRACT (MONTH FROM order_date) AS order_month,
     SUM(total_amount) AS monthly_revenue
 FROM orders
+-- This filters ensures only records year 2023 is produced
+WHERE EXTRACT(YEAR FROM order_date) = 2023
 GROUP BY order_year, order_month
 ORDER BY order_year, order_month;
 
- -- 
+-- ----------------------------------------------
+/* Find customers with no orders in the last 60 days relative to 2023-12-31 (i.e consider last active date
+up to 2023-12-31. Return customer_id, full_name, last_order_date */
+-- ----------------------------------------------
+
+WITH last_order_date AS (
+    -- the. query returns the highest order date as the last order date
+    SELECT customer_id,
+        MAX(order_date) AS last_order_date
+    FROM orders
+    GROUP BY customer_id
+)
+-- compares customer's last order if it's less than 2023-11-01 which is the assumed date 60 days before 2023-12-31
+SELECT c.customer_id,
+    c.full_name,
+    lod.last_order_date
+FROM customers c 
+LEFT JOIN last_order_date lod
+USING (customer_id)  
+WHERE lod.last_order_date <= '2023-11-01'
+    OR lod.last_order_date IS NULL
